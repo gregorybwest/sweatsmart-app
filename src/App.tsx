564e92bc83afcbb4c55d961a40fd97ad88811013
Stefan Webster <swebster08@gmail.com>
2024-05-09 20:51:25 -0700

    updating front end for call

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const userCall = async () => {
+    const response = await fetch(process.env.VERCEL_URL + "/api/users/1");
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <>
@@ -18,7 +24,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() =>
+            setCount((count) => {
+              count + 1;
+              userCall;
+            })
+          }
+        >
           count is {count}
         </button>
         <p>
@@ -29,7 +42,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
