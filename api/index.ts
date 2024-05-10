c5eaf33fd9d4633be9bd916e69a622a395b14fc6
Stefan Webster <swebster08@gmail.com>
2024-05-09 19:51:54 -0700

    route testing

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,10 @@ const app = express();
 
 app.get("/api", (req, res) => res.send("Express on Vercel"));
 
+app.get("/hello", (req, res) => res.send("Express on Vercel hello"));
+
+app.get("/api/hello", (req, res) => res.send("Express on Vercel api/hello"));
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
