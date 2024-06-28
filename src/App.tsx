a20204f4ebdb605ae30f35695658353519be8ce5
greg <gregorybwest@gmail.com>
2024-06-27 21:15:41 -0700

    setting up env variables for dev and prod

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import './App.css'
 async function sendCode(code: string) {
   const data = await axios.get(`https://sweatsmart-be.vercel.app/strava_auth?code=${code}`);
   console.log(data);
+  console.log(data.data.athlete.id, "fart");
 }
 
 function App() {
@@ -42,7 +43,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <button onClick={async () => {window.location.href = 'https://www.strava.com/oauth/authorize?client_id=120918&redirect_uri=https://sweatsmart-app.vercel.app&response_type=code&approval_prompt=auto&scope=activity:write,activity:read';}}>CLICK ME</button>
+      <button onClick={async () => {
+        window.location.href = `https://www.strava.com/oauth/authorize?client_id=120918&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&response_type=code&approval_prompt=auto&scope=activity:write,activity:read`;}}>CLICK ME</button>
       <p>{code}</p>
     </>
   )
