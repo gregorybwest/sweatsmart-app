9d4fd0809fb3901192808690cf7cab239f64f87d
Stefan Webster <swebster08@gmail.com>
2024-06-28 21:06:26 -0700

    More better looking

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import axios from 'axios'
-import './App.css'
+import { useEffect } from "react";
+import sweatSmartLogo from "/SweatSmartLogo.png";
+import stravaConnectBtn from "/strava_api_logos/btn_strava_connectwith_orange/btn_strava_connectwith_orange.svg";
+import axios from "axios";
+import "./App.css";
 
 async function sendCode(code: string) {
-  const data = await axios.get(`https://sweatsmart-be.vercel.app/strava_auth?code=${code}`);
+  const data = await axios.get(
+    `https://sweatsmart-be.vercel.app/strava_auth?code=${code}`
+  );
   console.log(data);
   console.log(data.data.athlete.id, "fart");
 }
 
 function App() {
-  const [count, setCount] = useState(0)
   const params = new URLSearchParams(document.location.search);
   const code = params.get("code");
 
@@ -24,30 +25,29 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="#" target="_blank">
+          <img
+            src={sweatSmartLogo}
+            className="logo react"
+            alt="SweatSmartlogo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to SweatSmart</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>Connect to Strava to get started</p>
+        <button
+          onClick={async () => {
+            window.location.href = `https://www.strava.com/oauth/authorize?client_id=120918&redirect_uri=${
+              import.meta.env.VITE_REDIRECT_URI
+            }&response_type=code&approval_prompt=auto&scope=activity:write,activity:read`;
+          }}
+        >
+          <img src={stravaConnectBtn} alt="Connect with Strava" />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button onClick={async () => {
-        window.location.href = `https://www.strava.com/oauth/authorize?client_id=120918&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&response_type=code&approval_prompt=auto&scope=activity:write,activity:read`;}}>CLICK ME</button>
-      <p>{code}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
