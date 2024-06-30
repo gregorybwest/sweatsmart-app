aaab035f167314045d463f24b782cdef71e9c154
Stefan Webster <swebster08@gmail.com>
2024-06-29 19:05:24 -0700

    More Readable

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import axios from "axios";
 import "./App.css";
 
 async function sendCode(code: string) {
-
-  const data = await axios.get(`https://sweatsmart-be.vercel.app/strava_auth?code=${code}`);
+  const data = await axios.get(
+    `https://sweatsmart-be.vercel.app/strava_auth?code=${code}`
+  );
   console.log(data["data"]);
 }
 
@@ -36,14 +37,13 @@ function App() {
       <div className="card">
         <p>Connect to Strava to get started</p>
         <button
+          id="strava-connect-btn"
           onClick={async () => {
             window.location.href = `https://www.strava.com/oauth/authorize?client_id=120918&redirect_uri=${
               import.meta.env.VITE_REDIRECT_URI
             }&response_type=code&approval_prompt=auto&scope=activity:write,activity:read`;
           }}
-        >
-          <img src={stravaConnectBtn} alt="Connect with Strava" />
-        </button>
+        ></button>
       </div>
     </>
   );
