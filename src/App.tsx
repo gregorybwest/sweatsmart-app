9c7b164588533c5839b25c889bcf0e7708f15339
greg <gregorybwest@gmail.com>
2024-07-11 19:14:07 -0700

    separate App.tsx into Home and Workouts pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Workouts from "./Pages/Workouts";
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/workouts" element={<Workouts/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,24 +1,7 @@
-import { useEffect } from "react";
-import sweatSmartLogo from "/sweatsmart-logo.svg";
-import axios from "axios";
 
-async function sendCode(code: string) {
-  const data = await axios.get(
-    `https://sweatsmart-be.vercel.app/strava_auth?code=${code}`
-  );
-  console.log(data["data"]);
-}
+import sweatSmartLogo from "/sweatsmart-logo.svg";
 
 function Home() {
-  const params = new URLSearchParams(document.location.search);
-  const code = params.get("code");
-
-  useEffect(() => {
-    if (code) {
-      sendCode(code);
-    }
-  }, [code]);
-
   return (
     <>
       <div>
@@ -38,7 +21,7 @@ function Home() {
           onClick={async () => {
             window.location.href = `https://www.strava.com/oauth/authorize?client_id=120918&redirect_uri=${
               import.meta.env.VITE_REDIRECT_URI
-            }&response_type=code&approval_prompt=auto&scope=activity:write,activity:read`;
+            }/workouts&response_type=code&approval_prompt=auto&scope=activity:write,activity:read`;
           }}
         ></button>
       </div>
