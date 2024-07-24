15ac86dd9bee018fecdb2e7718b979893bdea6be
greg <gregorybwest@gmail.com>
2024-07-23 20:49:56 -0700

    fix multiple calls and log athlete data

--- src/Pages/Workouts.tsx
+++ src/Pages/Workouts.tsx
@@ -4,33 +4,48 @@ import axios from "axios";
 
 
 async function sendCode(code: string) {
-  const data = await axios.get(
-    `https://sweatsmart-be.vercel.app/strava_auth?code=${code}`
-  );
-  console.log(data["data"]);
+  try {
+    const response = await axios.get(
+      `https://sweatsmart-be.vercel.app/strava_auth?code=${code}`
+    );
+    console.log(response.data);
+    return response.data; // Return the data
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null; // Handle error appropriately
+  }
 }
 
-// async function getStravaStats(data){
-//   const response = await axios.get(`https://www.strava.com/api/v3/athletes/${id}/stats`, {
-//     headers: {
-//       'Authorization': `Bearer ${token}`
-//     }
-//   });
-//   console.log(response.data);
-// }
+async function getStravaStats(athlete_id: number, token: string){
+  const response = await axios.get(`https://www.strava.com/api/v3/athletes/${athlete_id}/stats`, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  console.log(response.data);
+}
 
 function Workouts() {
   const params = new URLSearchParams(document.location.search);
   const code = params.get("code");
   const [data, setData] = useState({});
+  
 
 
   useEffect(() => {
     if (code) {
-      setData(sendCode(code));
+      sendCode(code).then(result => {
+        if (result) {
+          setData(result); // Set the data state with the fetched result
+          const athlete_id = result['athlete']['id'];
+          const access_token = result['access_token'];
+          localStorage.setItem("refresh_token", result['refresh_token'])
+          getStravaStats(athlete_id, access_token);
+        }
+      });
     }
     // getStravaStats(data);
-  }, [code, data]);
+  }, [code]); // Only depend on `code`
 
   return (
     <>
