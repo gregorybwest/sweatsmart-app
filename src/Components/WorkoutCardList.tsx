dff2095dfcac2f2178914a93358fde9120d5c4b1
greg <gregorybwest@gmail.com>
2024-09-10 21:38:52 -0700

    color gradient and centered cards on page

--- src/Components/WorkoutCardList.tsx
+++ src/Components/WorkoutCardList.tsx
@@ -9,10 +9,25 @@ interface WorkoutCardListProps {
 
 export const WorkoutCardList: React.FC<WorkoutCardListProps> = ({ easyRun, mediumRun, hardRun }) => {
   return (
-    <>
-      <WorkoutCard pace={easyRun.pace} time={easyRun.time} title={easyRun.title} className="bg-yellow-500" />
-      <WorkoutCard pace={mediumRun.pace} time={mediumRun.time} title={mediumRun.title} className="bg-green-500" />
-      <WorkoutCard pace={hardRun.pace} time={hardRun.time} title={hardRun.title} className="bg-red-500" />
-    </>
+    <div className="flex flex-col items-center">
+      <WorkoutCard
+        pace={easyRun.pace}
+        time={easyRun.time}
+        title={easyRun.title}
+        className="bg-gradient-to-b from-yellow-300 to-yellow-600 mb-4 mt-4 transition-transform transform hover:scale-105 hover:z-10 hover:origin-center max-w-screen-lg mx-auto"
+      />
+      <WorkoutCard
+        pace={mediumRun.pace}
+        time={mediumRun.time}
+        title={mediumRun.title}
+        className="bg-gradient-to-b from-green-400 to-green-600 mb-4 transition-transform transform hover:scale-105 hover:z-10 hover:origin-center max-w-screen-lg mx-auto"
+      />
+      <WorkoutCard
+        pace={hardRun.pace}
+        time={hardRun.time}
+        title={hardRun.title}
+        className="bg-gradient-to-b from-red-400 to-red-600 transition-transform transform hover:scale-105 hover:z-10 hover:origin-center max-w-screen-lg mx-auto"
+      />
+    </div>
   );
 };
