f589490c74954294d214eb48add09d387a44e96f
Greg West <gregorybwest@gmail.com>
2024-10-10 21:10:46 -0700

    ability to click on sport tabs to toggle between type of sport suggestions

--- src/Components/WorkoutCardList.tsx
+++ src/Components/WorkoutCardList.tsx
@@ -2,9 +2,7 @@ import { WorkoutCard } from "./WorkoutCard";
 import { Run, Ride, Swim } from "../Pages/Workouts";
 
 interface WorkoutCardListProps {
-  runs: Run[];
-  rides: Ride[];
-  swims: Swim[];
+  workouts: (Run | Ride | Swim)[]; // A generic array of workouts
 }
 
 const difficultyColors: { [key: string]: string } = {
@@ -14,62 +12,23 @@ const difficultyColors: { [key: string]: string } = {
   featured: "bg-gradient-to-b from-purple-400 to-purple-600",
 };
 
-export const WorkoutCardList: React.FC<WorkoutCardListProps> = ({ runs, rides, swims }) => {
-
+export const WorkoutCardList: React.FC<WorkoutCardListProps> = ({ workouts }) => {
   return (
     <>
-      {runs.length === 0 ? (
+      {workouts.length === 0 ? (
         <div>Loading</div>
-      ) : typeof runs[0] === "string" ? (
-        <div>{runs[0]}</div>
+      ) : typeof workouts[0] === "string" ? (
+        <div>{workouts[0]}</div>
       ) : (
         <div className="flex flex-col items-center">
-          {runs.map((run, index) => (
+          {workouts.map((workout, index) => (
             <WorkoutCard
               key={index}
-              pace={run.pace}
-              time={run.time}
-              title={run.title}
+              pace={workout.pace}
+              time={workout.time}
+              title={workout.title}
               className={`${
-                difficultyColors[run.difficulty]
-              } mb-4 mt-4 transition-transform transform hover:z-10 hover:origin-center max-w-screen-lg mx-auto`}
-            />
-          ))}
-        </div>
-      )}
-            {rides.length === 0 ? (
-        <div>Loading</div>
-      ) : typeof rides[0] === "string" ? (
-        <div>{rides[0]}</div>
-      ) : (
-        <div className="flex flex-col items-center">
-          {rides.map((run, index) => (
-            <WorkoutCard
-              key={index}
-              pace={run.pace}
-              time={run.time}
-              title={run.title}
-              className={`${
-                difficultyColors[run.difficulty]
-              } mb-4 mt-4 transition-transform transform hover:z-10 hover:origin-center max-w-screen-lg mx-auto`}
-            />
-          ))}
-        </div>
-      )}
-            {swims.length === 0 ? (
-        <div>Loading</div>
-      ) : typeof swims[0] === "string" ? (
-        <div>{swims[0]}</div>
-      ) : (
-        <div className="flex flex-col items-center">
-          {swims.map((run, index) => (
-            <WorkoutCard
-              key={index}
-              pace={run.pace}
-              time={run.time}
-              title={run.title}
-              className={`${
-                difficultyColors[run.difficulty]
+                difficultyColors[workout.difficulty]
               } mb-4 mt-4 transition-transform transform hover:z-10 hover:origin-center max-w-screen-lg mx-auto`}
             />
           ))}
