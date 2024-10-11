bde67b6ff115f7aeef64830bb1342395eee41841
Stefan Webster <swebster08@gmail.com>
2024-10-10 20:23:26 -0700

    Getting multi-sport a bit further

--- src/Pages/Workouts.tsx
+++ src/Pages/Workouts.tsx
@@ -1,106 +1,135 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { WorkoutCardList } from "../Components/WorkoutCardList";
-import { NavBar } from "../Components/NavBar";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { WorkoutCardList } from '../Components/WorkoutCardList';
+import { NavBar } from '../Components/NavBar';
 
 export interface Run {
-  pace: number;
-  time: number;
-  title: string;
-  suggested: boolean;
-  difficulty: string;
+    pace: number;
+    time: number;
+    title: string;
+    suggested: boolean;
+    difficulty: string;
 }
 
-// interface Runs {
-//   message?: string;
-//   runs?: Run[];
-// }
+export interface Ride {
+    pace: number;
+    time: number;
+    title: string;
+    suggested: boolean;
+    difficulty: string;
+}
+
+export interface Swim {
+    pace: number;
+    time: number;
+    title: string;
+    suggested: boolean;
+    difficulty: string;
+}
 
 const backEndURI = import.meta.env.VITE_BACKEND_URI;
 
 async function sendCode(code: string) {
-  try {
-    const response = await axios.get(`${backEndURI}/strava_auth?code=${code}`);
-    return response.data; // Return the data
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null; // Handle error appropriately
-  }
+    try {
+        const response = await axios.get(
+            `${backEndURI}/strava_auth?code=${code}`
+        );
+        return response.data; // Return the data
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null; // Handle error appropriately
+    }
 }
 
 async function sendRefreshToken(refreshToken: string) {
-  try {
-    const response = await axios.get(`${backEndURI}/strava_auth?refreshToken=${refreshToken}`);
-    return response.data; // Return the data
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null; // Handle error appropriately
-  }
+    try {
+        const response = await axios.get(
+            `${backEndURI}/strava_auth?refreshToken=${refreshToken}`
+        );
+        return response.data; // Return the data
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null; // Handle error appropriately
+    }
 }
 
-async function getStravaStats(athleteId: number | string | null, accessToken: string, setruns: (runs: Run[]) => void) {
-  const stravaStats = await axios.get(`${backEndURI}/strava_stats`, {
-    params: {
-      athleteId: athleteId,
-      accessToken: accessToken,
-    },
-  });
-  console.log("Strava stats", stravaStats);
-  setruns(stravaStats.data.workouts.runs);
+async function getStravaStats(
+    athleteId: number | string | null,
+    accessToken: string,
+    setRuns: (runs: Run[]) => void,
+    setRides: (rides: Ride[]) => void,
+    setSwims: (swims: Swim[]) => void
+) {
+    const stravaStats = await axios.get(`${backEndURI}/strava_stats`, {
+        params: {
+            athleteId: athleteId,
+            accessToken: accessToken,
+        },
+    });
+    setRuns(stravaStats.data.workouts.runs);
+    setRides(stravaStats.data.workouts.rides);
+    setSwims(stravaStats.data.workouts.swims);
 }
 
 function Workouts() {
-  const params = new URLSearchParams(document.location.search);
-  const code = params.get("code");
-  const [data, setData] = useState({});
-  const [runs, setruns] = useState<Run[]>([]);
-  console.log(data);
+    const params = new URLSearchParams(document.location.search);
+    const code = params.get('code');
+    const [runs, setRuns] = useState<Run[]>([]);
+    const [rides, setRides] = useState<Ride[]>([]);
+    const [swims, setSwims] = useState<Swim[]>([]);
 
-  useEffect(() => {
-    const refreshToken = localStorage.getItem("refreshToken");
-    if (refreshToken) {
-      sendRefreshToken(refreshToken).then((result) => {
-        if (result) {
-          setData(result); // Set the data state with the fetched result
-          const athleteId = localStorage.getItem("athleteId");
-          const accessToken = result["accessToken"];
-          getStravaStats(athleteId, accessToken, setruns);
+    useEffect(() => {
+        const refreshToken = localStorage.getItem('refreshToken');
+        if (refreshToken) {
+            sendRefreshToken(refreshToken).then((result) => {
+                if (result) {
+                    const athleteId = localStorage.getItem('athleteId');
+                    const accessToken = result['accessToken'];
+                    getStravaStats(athleteId, accessToken, setRuns, setRides, setSwims);
+                }
+            });
+        } else if (code) {
+            sendCode(code).then((result) => {
+                if (result) {
+                    const athleteId = result['athlete']['id'];
+                    const accessToken = result['accessToken'];
+                    localStorage.setItem(
+                        'refreshToken',
+                        result['refreshToken']
+                    );
+                    localStorage.setItem('athleteId', athleteId);
+                    getStravaStats(
+                        athleteId,
+                        accessToken,
+                        setRuns,
+                        setRides,
+                        setSwims
+                    );
+                }
+            });
         }
-      });
-    } else if (code) {
-      sendCode(code).then((result) => {
-        if (result) {
-          setData(result); // Set the data state with the fetched result
-          const athleteId = result["athlete"]["id"];
-          const accessToken = result["accessToken"];
-          localStorage.setItem("refreshToken", result["refreshToken"]);
-          localStorage.setItem("athleteId", athleteId);
-          getStravaStats(athleteId, accessToken, setruns);
-        }
-      });
-    }
-    // getStravaStats(data);
-  }, [code]); // Only depend on `code`
+        // getStravaStats(data);
+    }, [code]); // Only depend on `code`
 
-  return (
-    <>
-      <NavBar />
-      <div className="flex-none">
-        <div role="tablist" className="tabs tabs-boxed">
-          <a role="tab" className="tab hover:tab-active">
-            Run
-          </a>
-          <a role="tab" className="tab hover:tab-active">
-            Bike
-          </a>
-          <a role="tab" className="tab hover:tab-active">
-            Swim
-          </a>
-        </div>
-      </div>
-      <WorkoutCardList runs={runs} />
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            <div className='flex-none'>
+                <div role='tablist' className='tabs tabs-boxed'>
+                    <a role='tab' className='tab hover:tab-active'>
+                        Run
+                    </a>
+                    <a role='tab' className='tab hover:tab-active'>
+                        Bike
+                    </a>
+                    <a role='tab' className='tab hover:tab-active'>
+                        Swim
+                    </a>
+                </div>
+            </div>
+            <WorkoutCardList runs={runs} rides={rides} swims={swims}/>
+        </>
+    );
 }
 
 export default Workouts;
