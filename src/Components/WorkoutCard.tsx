629a0fa03ff58e374e7d28338001b05d94f33093
Greg West <gregorybwest@gmail.com>
2024-10-16 20:46:21 -0700

    move average pace readable format calculation to backend

--- src/Components/WorkoutCard.tsx
+++ src/Components/WorkoutCard.tsx
@@ -10,12 +10,7 @@ export const WorkoutCard: React.FC<WorkoutCardProps> = ({ pace, time, className,
     <div className={`card text-primary-content w-96 rounded-lg shadow-lg w-full ${className}`}>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p>
-          Pace: {Math.floor(pace / 60)}:
-          {Math.floor(pace % 60)
-            .toString()
-            .padStart(2, "0")}
-        </p>
+        <p>Pace: {pace}</p>
         <p>Average Time: {Math.floor(time / 60)}:00</p>
       </div>
     </div>
